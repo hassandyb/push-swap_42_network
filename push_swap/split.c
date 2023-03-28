74107c1a8263cfa58c281fe34e56cec3a09970ca
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-03-28 18:49:21 +0000

    Add files via upload

--- push_swap/split.c
+++ push_swap/split.c
@@ -6,7 +6,7 @@
 /*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 16:56:24 by hed-dyb           #+#    #+#             */
-/*   Updated: 2023/03/28 17:28:38 by hed-dyb          ###   ########.fr       */
+/*   Updated: 2023/03/28 18:48:47 by hed-dyb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,39 @@ int	ft_count(char *stock)
 {
 	int	count;
 	int	h;
+
+	h = 0;
+	while(stock[h] == ' ')
+		h++;
 	while(stock[h])
 	{
-		
-
+		if((stock[h] == ' ' && stock[h + 1] != ' ') || stock[h + 1] == '\0')
+			count++;
 		h++;
 	}
-
-
+	return (count);
 }
 char **ft_split(char *stock)
 {
-	int i;
+	int y;
+	int	x;
 	int n;
 	char **split;
 	
 	n = ft_count(stock);
-	split = (char **)malloc(sizeof(char *));
-	i = 0;
-	
-	while()
+	split = (char **)malloc((n + 1) * sizeof(char *));
+	if(split == NULL)
+	{
+		free (stock);
+		exit (1);
+	}
+	x = 0;
+	y = 0;
+	while(y < n && stock[x] != '\0')
+	{
+		while(stock[x] = ' ')// here
+			x++;
+		begin = x;
+	}
 }
+
