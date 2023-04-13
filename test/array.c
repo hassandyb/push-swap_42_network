5e6d3d6188533923a460cacc959f2fb9ff3af950
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-04-13 17:21:40 +0000

    Add files via upload

---
+++ test/array.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   array.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/02 14:23:08 by hed-dyb           #+#    #+#             */
+/*   Updated: 2023/04/13 17:06:58 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int ft_arraylen(char **split)
+{
+	int	l;
+
+	l = 0;
+	while(split[l])
+		l++;
+	return (l);
+}
+
+int ft_atoi(char *s, char **to_free)
+{
+	int i;
+	int sign;
+	long result;
+	
+	result = 0;
+	sign = 1;
+	i = 0;
+	if(s[0] == '-' || s[0] == '+')
+	{
+		if(s[0] == '-')
+			sign = -1;
+		i++;
+	}
+	if (s[i] == '\0')// in case you have - or + only
+		ft_error(to_free, "Error :\nArg containing - or + only!");
+	while(s[i] >= '0' && s[i] <= '9')
+	{
+		result = (result * 10) + s[i] - '0';
+		i++;
+	}
+	result = result * sign;
+	if(result < -2147483648 || result > 2147483647)
+		ft_error(to_free, "Error :\nInt coudn't hold a number!");
+	return (result);
+}
+
+void	ft_no_doubles(int *numbers, int len)
+{
+	int j;
+	int	i;
+	int count;
+
+	
+	j = 0;
+	while(j < len)
+	{
+		count = 0;
+		i = 0;
+		while(i < len)
+		{
+			if(numbers[j] == numbers[i])
+				count++;
+			i++;
+		}
+		if(count > 1)
+		{
+			write(2, "Error : \nDouble number!", 24);
+			exit (0);
+		}
+		j++;
+	}
+}
+
+void	ft_not_sorted(int *numbers, int len)
+{
+	int i = 0;
+	while(i < len - 1)
+	{
+		if(numbers[i] > numbers[i+1])
+			return;
+		i++;
+	}
+	write(2, "Error :\nNumbers are already sorted - or one number !",53);
+	free(numbers);
+	exit (1);
+}
+
+int *ft_creat_array(char **split, int len)
+{
+	int	i;
+	int	*numbers;
+
+	i = 0;
+	numbers = (int *)malloc(len * sizeof(int));
+	if(numbers == NULL)
+	{
+		ft_free_double_pointer(split);
+		exit (1);
+	}
+	while(split[i])
+	{
+		numbers[i] = ft_atoi(split[i], split);
+		i++;
+	}
+	ft_free_double_pointer(split);
+	ft_no_doubles(numbers, len);
+	ft_not_sorted(numbers, len);
+	return (numbers);
+}
---
+++ test/instractions.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instractions.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 18:01:43 by hed-dyb           #+#    #+#             */
+/*   Updated: 2023/04/12 18:23:43 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+
+
+
+void ft_push(t_stack **src, t_stack **dst, char *msg)
+{
+	t_stack *src_begin;
+	if(*src == NULL || src == NULL)
+		return;
+
+	src_begin = *src;
+
+	*src = (*src)->next;
+	src_begin->next = *dst;
+	*dst = src_begin;
+	ft_putstr(msg);
+}
+
+void ft_swap(t_stack **ptr, char *msg)//swap tow first elemnt in the linked list
+{
+	t_stack *swap;
+	
+	if(*ptr == NULL || (*ptr)->next == NULL)
+		return;
+	swap = (*ptr)->next;
+
+	(*ptr)->next = (*ptr)->next->next;
+	swap->next = (*ptr);
+	(*ptr) = swap;
+	if (msg)
+		ft_putstr(msg);
+}
+
+void ft_rotate(t_stack **ptr, char *msg)// make the first become the last
+{
+	t_stack *begin;
+	t_stack *temp;
+	if(ptr == NULL  || *ptr == NULL || (*ptr)->next == NULL)
+		return ;
+	begin = (*ptr)->next;
+	temp = (*ptr);
+	while(temp)
+	{
+		if(temp->next == NULL)
+			break ;
+		else
+			temp = temp->next;
+	}
+	temp->next = *ptr;
+	(*ptr)->next = NULL;
+	(*ptr) = begin;
+	if (msg)
+		ft_putstr(msg);
+}
+
+void ft_rr(t_stack **a, t_stack **b)
+{
+	if (!*a || !*b || !(*a)->next || !(*b)->next)
+		return ;
+	ft_rotate(a, NULL);
+	ft_rotate(b, NULL);
+	ft_putstr("rr\n");
+}
+
+void ft_reverse_rotate(t_stack **ptr, char *msg)// make the last become the first
+{
+	t_stack *begin;
+	t_stack *befor_last;
+	t_stack *temp;
+	
+	if(*ptr == NULL || (*ptr)->next == NULL)
+		return ;
+	begin = *ptr;
+	while(ptr)
+	{
+		if((*ptr)->next->next == NULL)
+		{
+			befor_last = (*ptr);
+			break ;
+		}
+		(*ptr) = (*ptr)->next;
+	}
+	(*ptr) = (*ptr)->next;
+
+	(*ptr)->next = begin;
+	befor_last->next = NULL;
+	if (msg)
+		ft_putstr(msg);
+
+}
