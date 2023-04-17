0c4218f305ec6f23e55cfe1c526e51364c1214aa
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2023-04-17 15:56:20 +0000

    Add files via upload

---
+++ 111/final_sort.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   final_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 21:13:11 by hed-dyb           #+#    #+#             */
+/*   Updated: 2023/04/16 15:48:22 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_maxnumber_indice(t_stack *list, int len)
+{
+	int	i;
+
+	i = 0;
+	while (list)
+	{
+		if (list->rank == len - 1)
+		{
+			return (i);
+		}
+		list = list->next;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_normal_case(t_stack **b, t_stack **a, int len, int indice)
+{
+	if ((*b)->rank == len - 1)
+	{
+		ft_push(b, a, "pa\n");
+		len--;
+	}
+	else if (indice < len / 2)
+		ft_rotate(b, "rb\n");
+	else if (indice >= len / 2)
+		ft_reverse_rotate(b, "rrb\n");
+	return (len);
+}
+
+void	ft_push_2_in_right(t_stack **b, t_stack **a, int len)
+{
+	while (*b)
+	{
+		if ((*b)->rank == len - 2)
+		{
+			ft_push(b, a, "pa\n");
+			break ;
+		}
+		else
+			ft_rotate(b, "rb\n");
+	}
+	while (*b)
+	{
+		if ((*b)->rank == len - 1)
+		{
+			ft_push(b, a, "pa\n");
+			break ;
+		}
+		else
+			ft_rotate(b, "rb\n");
+	}
+	ft_swap(a, "sa\n");
+}
+
+void	ft_push_2_in_left(t_stack **b, t_stack **a, int len)
+{
+	while (*b)
+	{
+		if ((*b)->rank == len - 2)
+		{
+			ft_push(b, a, "pa\n");
+			break ;
+		}
+		else
+			ft_reverse_rotate(b, "rrb\n");
+	}
+	while (*b)
+	{
+		if ((*b)->rank == len - 1)
+		{
+			ft_push(b, a, "pa\n");
+			break ;
+		}
+		else
+			ft_reverse_rotate(b, "rrb\n");
+	}
+	ft_swap(a, "sa\n");
+}
+
+void	ft_final_sort(t_stack **b, t_stack **a, int len)
+{
+	int	indice;
+	int	befor_max_indice;
+
+	while (*b)
+	{
+		indice = ft_maxnumber_indice(*b, len);
+		befor_max_indice = ft_maxnumber_indice(*b, len - 1);
+		if (indice < len / 2 && befor_max_indice < len / 2
+			&& befor_max_indice < indice && len > 3)
+		{
+			ft_push_2_in_right(b, a, len);
+			len = len - 2;
+		}
+		else if (indice > len / 2 && befor_max_indice > len / 2
+			&& befor_max_indice > indice && len > 3)
+		{
+			ft_push_2_in_left(b, a, len);
+			len = len - 2;
+		}
+		else
+			len = ft_normal_case(b, a, len, indice);
+	}
+}
---
+++ 111/instractions.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instractions.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 18:01:43 by hed-dyb           #+#    #+#             */
+/*   Updated: 2023/04/16 15:50:36 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_push(t_stack **src, t_stack **dst, char *msg)
+{
+	t_stack	*src_begin;
+
+	if (*src == NULL || src == NULL)
+		return ;
+	src_begin = *src;
+	*src = (*src)->next;
+	src_begin->next = *dst;
+	*dst = src_begin;
+	ft_putstr(msg);
+}
+
+void	ft_swap(t_stack **ptr, char *msg)
+{
+	t_stack	*swap;
+
+	if (*ptr == NULL || (*ptr)->next == NULL)
+		return ;
+	swap = (*ptr)->next;
+	(*ptr)->next = (*ptr)->next->next;
+	swap->next = (*ptr);
+	(*ptr) = swap;
+	if (msg)
+		ft_putstr(msg);
+}
+
+void	ft_rotate(t_stack **ptr, char *msg)
+{
+	t_stack	*begin;
+	t_stack	*temp;
+
+	if (ptr == NULL || *ptr == NULL || (*ptr)->next == NULL)
+		return ;
+	begin = (*ptr)->next;
+	temp = (*ptr);
+	while (temp)
+	{
+		if (temp->next == NULL)
+			break ;
+		else
+			temp = temp->next;
+	}
+	temp->next = *ptr;
+	(*ptr)->next = NULL;
+	(*ptr) = begin;
+	if (msg)
+		ft_putstr(msg);
+}
+
+void	ft_reverse_rotate(t_stack **ptr, char *msg)
+{
+	t_stack	*begin;
+	t_stack	*befor_last;
+
+	if (*ptr == NULL || (*ptr)->next == NULL)
+		return ;
+	begin = *ptr;
+	befor_last = NULL;
+	while (*ptr)
+	{
+		if ((*ptr)->next->next == NULL)
+		{
+			befor_last = (*ptr);
+			break ;
+		}
+		(*ptr) = (*ptr)->next;
+	}
+	(*ptr) = (*ptr)->next;
+	(*ptr)->next = begin;
+	befor_last->next = NULL;
+	if (msg)
+		ft_putstr(msg);
+}
